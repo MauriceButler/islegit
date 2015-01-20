08eb8070d6476959792b4e95560b7938240c033c
mauricebutler <maurice.butler@gmail.com>
2015-01-20 15:58:58 +1000

    inital commit

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # islegit
-CLI to check if all installed modules are up to date as per dependencies SemVer
+
+Check if all installed modules are up to date as per dependencies SemVer
+
+## CLI usage
+
+    islegit
+
+Will exit with exit code 1 if there is anything that could be updated with npm update, otherwise 0.
+
+
+## Module
+
+    // TODO: If I ever need it...
---
+++ index.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+var npm = require('npm');
+
+npm.load({loglevel: 'silent'}, function() {
+    npm.commands.outdated([], true, function(error, results){
+        for (var i = 0; i < results.length; i++) {
+            var result = results[i];
+
+            if(result[2] !== result[3]){
+                process.exit(1);
+            }
+        }
+    });
+});
